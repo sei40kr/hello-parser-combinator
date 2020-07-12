de896f3b8e904635de2d45d027544081fb2260dd
sei40kr <sei40kr@gmail.com>
2020-07-13 00:47:12 +0900

    Implement satisfy

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,9 @@
 module Lib
   ( anyChar
+  , satisfy
   )
 where
 
 anyChar (x : xs) = (x, xs)
+
+satisfy f (x : xs) | f x = (x, xs)
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -3,6 +3,7 @@ module LibSpec
   )
 where
 
+import           Data.Char
 import           Test.Hspec
 import           Test.QuickCheck
 
@@ -12,3 +13,6 @@ spec :: Spec
 spec = do
   describe "anyChar" $ do
     it "test1" $ anyChar "abc" `shouldBe` ('a', "bc")
+  describe "satisfy" $ do
+    it "test1" $ satisfy (== 'a') "abc" `shouldBe` ('a', "bc")
+    it "test1" $ satisfy isDigit "123" `shouldBe` ('1', "23")
