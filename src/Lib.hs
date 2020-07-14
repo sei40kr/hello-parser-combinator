ed2497ced2dcc72508454ef719f1c4de61148c37
sei40kr <sei40kr@gmail.com>
2020-07-15 00:10:14 +0900

    Return the last state on a parse failure

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,18 +17,20 @@ import           Control.Monad.State
 
 anyChar = StateT anyChar where
   anyChar (x : xs) = Right (x, xs)
-  anyChar _        = Left "too short"
+  anyChar xs       = Left ("too short", xs)
 
 satisfy f = StateT satisfy where
-  satisfy (x : xs) | not $ f x = Left $ ": " ++ show x
+  satisfy (x : xs) | not $ f x = Left (": " ++ show x, x : xs)
   satisfy xs                   = runStateT anyChar xs
 
-(StateT a) <|> (StateT b) = StateT $ \s -> a s <|> b s where
-  Left a <|> Left b = Left $ b ++ a
-  Left _ <|> b      = b
-  a      <|> _      = a
+(StateT a) <|> (StateT b) = StateT f where
+  f s0 = a s0 <|> b s0   where
+    Left (a, s1) <|> _ | s0 /= s1 = Left (a, s1)
+    Left (a, _) <|> Left (b, s2)  = Left (b ++ a, s2)
+    Left _      <|> b             = b
+    a           <|> _             = a
 
-left = lift . Left
+left e = StateT $ \s -> Left (e, s)
 
 char c = satisfy (== c) <|> left ("not char " ++ show c)
 digit = satisfy isDigit <|> left "not digit"
--- test/LibSpec.hs
+++ test/LibSpec.hs
@@ -15,16 +15,18 @@ spec :: Spec
 spec = do
   describe "anyChar" $ do
     it "test1" $ runStateT anyChar "abc" `shouldBe` Right ('a', "bc")
-    it "test2" $ runStateT anyChar "" `shouldBe` Left "too short"
+    it "test2" $ runStateT anyChar "" `shouldBe` Left ("too short", "")
   describe "char" $ do
     it "test1" $ runStateT (char 'a') "abc" `shouldBe` Right ('a', "bc")
-    it "test2" $ runStateT (char 'a') "123" `shouldBe` Left "not char 'a': '1'"
+    it "test2" $ runStateT (char 'a') "123" `shouldBe` Left
+      ("not char 'a': '1'", "123")
   describe "digit" $ do
     it "test1" $ runStateT digit "123" `shouldBe` Right ('1', "23")
-    it "test2" $ runStateT digit "abc" `shouldBe` Left "not digit: 'a'"
+    it "test2" $ runStateT digit "abc" `shouldBe` Left ("not digit: 'a'", "abc")
   describe "letter" $ do
     it "test1" $ runStateT letter "abc" `shouldBe` Right ('a', "bc")
-    it "test2" $ runStateT letter "123" `shouldBe` Left "not letter: '1'"
+    it "test2" $ runStateT letter "123" `shouldBe` Left
+      ("not letter: '1'", "123")
   describe "many" $ do
     it "test1" $ evalStateT (many letter) "abc123" `shouldBe` Right "abc"
     it "test2" $ evalStateT (many letter) "123abc" `shouldBe` Right ""
